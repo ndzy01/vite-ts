1936c94f343edc4ed8e62d1affa36bedc2cfb3c9
张一 <2326962137@qq.com>
2022-08-31 16:07:41 +0800

    chore 深度优先&广度优先

---
+++ src/dfs&bfs/二叉树的中序遍历.ts
@@ -0,0 +1,15 @@
+function inorderTraversal(root: TreeNode | null): number[] {
+  const stack: number[] = [];
+
+  const loop = (tree: TreeNode | null) => {
+    if (tree) {
+      loop(tree.left);
+      stack.push(tree.val);
+      loop(tree.right);
+    }
+  };
+
+  loop(root);
+
+  return stack;
+}
---
+++ src/dfs&bfs/二叉树的层序遍历.ts
@@ -0,0 +1,42 @@
+function levelOrder(root: TreeNode | null): number[][] {
+  const res: number[][] = [];
+
+  if (!root) {
+    return res;
+  }
+
+  const queue: TreeNode[] = [];
+
+  queue.push(root);
+
+  while (queue.length) {
+    const currentLevelSize = queue.length;
+    // 预存一个空数组 存储层节点数据
+    res.push([]);
+
+    for (let i = 1; i <= currentLevelSize; ++i) {
+      const node = queue.shift();
+
+      if (node) {
+        res[res.length - 1].push(node.val);
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+      }
+    }
+  }
+
+  return res;
+}
+
+const res = levelOrder({
+  val: 3,
+  left: { val: 9, left: null, right: null },
+  right: {
+    val: 20,
+    left: { val: 15, left: null, right: null },
+    right: { val: 7, left: null, right: null },
+  },
+});
+
+console.log(res);
