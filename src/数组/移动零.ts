73615f2fc68d5aac889e6c189a59689e3b7aa3fd
张一 <2326962137@qq.com>
2022-09-07 17:22:08 +0800

    chore 数组

---
+++ src/数组/移动零.ts
@@ -0,0 +1,18 @@
+function moveZeroes(nums: number[]): number[] {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      // 遇到非 0 元素，让 nums[j] = nums[i]，然后 j++
+      nums[j] = nums[i];
+      j++;
+    }
+  }
+
+  for (let i = j; i < nums.length; i++) {
+    // 剩下的元素全是0
+    nums[i] = 0;
+  }
+
+  return nums;
+}
