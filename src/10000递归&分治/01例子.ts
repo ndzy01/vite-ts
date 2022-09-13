6506eee77bce570bbd3ca981abde85559b388bd0
张一 <2326962137@qq.com>
2022-09-13 19:10:21 +0800

    chore 递归&分治

--- src/10000递归&分治/01例子.ts
+++ src/10000递归&分治/01例子.ts
@@ -1,13 +1,15 @@
 // n 的阶乘
-function factorial(n: number): number {
+const factorial = (n: number): number => {
   if (n <= 1) return 1;
 
   return n * factorial(n - 1);
-}
+};
 
 // 斐波拉切数列
-function fib(n: number): number {
+const fib = (n: number): number => {
   if (n <= 1) return n;
 
   return fib(n - 1) + fib(n - 2);
-}
+};
+
+console.log(factorial(4), fib(5));
